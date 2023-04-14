c2d3078537a0edb496aab2fcdee3b0cbb251e102
onkere-pvcc <126129300+onkere-pvcc@users.noreply.github.com>
2023-04-14 00:18:07 -0400

    Add files via upload

--- pizza.py
+++ pizza.py
@@ -1,73 +1,53 @@
 # Name: Omwira Nkere
-# Prog Purpose: This program computes colleged tuition & fees based on number of credits
-#   PVCCFee Ratres are from://www.edu/tuition-and-fees
-#   NOTE: All fees are PER CREDIT
-#       In-state tuition:$155, Out-of-state tuition: $331.60
-#       Capital fee: 23.50 (out-of-state Students only)
-#       Institution fee: $1.75
-#       Activity fee: $2.90
+# Prog Purpose: This program finds the cost and the size of a pizza
+# pizza cost:[Small:9.99, medium:12.99, large: 14.99, extra_large: 17.99]
+# Sales tax rate: 5.5%     
 
 import datetime
 
-#define tuition & fee rates
-RATE_TUITION_IN = 155
-RATE_TUITION_OUT = 331.60
-RATE_CAPITAL = 23.5
-RATE_INSTITUTION = 1.75
-RATE_ACTIVITY = 2.90
+#define pizza size, price and tax rate
+if small: pizza_price(9.99)
+if pizza_size == medium: pizza_price (12.99)
+if pizza_size == large: pizza_price (14.99)
+if pizza_size == extra_large: pizza_price(17.99)
+sales_tax_rate = 5.5
 
 #define global variables
-inout = 1 # 1 means in_state, 2 means oolut_of_ state
-numcredits = 0
-scholarshipamt = 0
-tuitionfee = 0
-capitalfee = 0
-institutionfee = 0
-activityfee = 0
-totalowed = 0
-balance = 0
+size = 1 # S means Small, M means medium, L means large & X means extra_large
+pizza_size = 0 
+pizza_cost = 0
+numpizza = 0
+
 
 ############## define program functions ######################
 def main():
-    get_user_data()
-    perform_calculations()
-    display_results()
+        get_user_data()
+        perform_calculations()
+        display_results()
 
-    def get_user_data():
-        global inout, numcredits, scholarshipamt
-        inout = int(input("Enter a 1 for IN-STATE; enter a 2 for OUT-OF-STATE:1"))
-        numcredits = int(input("Number of credits registered for:6"))
-        scholarshipamt = float(input("Scholarship amount received:800"))
+def get_user_data():
+        global pizza_size, numpizza
+        pizza_size = int(input("Enter S for small; enter M for medium; enter L for large; enter X for extra_large:"))
+        numpizza = int(input("Number of pizza for:"))
 
-    def perform_calculations():
-        global tuitionfee, capitalfee, institutionfee, activityfee, totalowed, balance
-        if inout == 1:
-            tuitionfee = numcredits * RATE_TUITION_IN
-            capitalfee = 0
-        else:
-            tuitionfee = numcredits * RATE_TUITION_OUT
-            capitalfee = numcredits * RATE_CAPITAL
-
-        institutionfee = numcredits * RATE_INSTITUTION
-        activityfee =numcredits * RATE_ACTIVITY
-        totalowed =tuitionfee + capitalfee + institutionfee + activityfee
-        balance = totalowed - scholarshipamt
-
+def perform_calculations():
+        global pizza_cost, sales_tax
+        pizza_cost = numpizza * pizza_price
+        sales_tax = pizza_cost * sales_tax_rate
+        total = Pizza_cost + sales_tax
+        
 def display_results():
     print('\n-----------------------------------------')
-    print('Number of credits : 6 ' + str(numcredits))
-    print('---------------------------------------')
-    print('tuition           $ ' + format(tuitionfee,'10,.2f'))
-    print('Capital Fee       $ ' + str(capitalfee))
-    print('Institution Fee   S ' + str(institutionfee))
-    print('Activity Fee      $ ' + str(activityfee))
-    print('Total             $ ' + str(totalowed))
-    print('Scholarship       $800 ' + str(scholarshipamt))
-    print('---------------------------------------')
-    print('Balance Owed      $ ' + str(balance))
+    print('*************** PALERMO PIZZA **************')
+    print('\n****THE BEST PLACE FOR THE BEST PIZZA*****')
+    print('--------------------------------------------')
+    print('pizza         $ ' + format(pizza_cost,'10,.2f'))
+    print('Sales Tax     $ ' + str(sales_tax))
+    print('Total         $ ' + str(total))
     print('---------------------------------------')
     print(str(datetime.datetime.now()))
-    print("NOTE: PVCC Fee Rates: https://ww.pvcc.edu/tuition-and-fees")
+    print("NOTE: PVCC Fee Rates: https://www.pvcc.edu/tuition-and-fees")
 
 
 ##########  call on main program to execute ###########
+main()
